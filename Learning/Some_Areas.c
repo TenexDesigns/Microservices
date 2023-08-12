2859fed6f206a1dab63a3c93226b75fcb7e9e70d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-12 12:22:54 +0300

    Create Some_Areas.c

---
+++ Learning/Some_Areas.c
@@ -0,0 +1,27 @@
+Microservices is an architectural style that structures an application as a collection of services that are highly maintainable and testable, loosely coupled, independently deployable, organized around business capabilities, and owned by a small team. The microservice architecture enables the rapid, frequent and reliable delivery of large, complex applications. It also enables an organization to evolve its technology stack [Source 1](https://microservices.io/).
+
+Here are some concepts you need to learn in microservices:
+
+- Decentralized Data Management: Each microservice has its own private database to ensure that the database schema can evolve without coordination.
+- Infrastructure Automation: Use of Continuous Delivery and DevOps to handle a large number of services efficiently.
+- Design for failure: The application needs to be resilient to failure given that services can fail at any time.
+- Evolutionary Design: The microservice architecture allows for an application to be developed and evolved by many teams [Source 1](https://microservices.io/).
+
+To implement these concepts, you need to understand how to design, develop and deploy each microservice independently. You can use different programming languages for different microservices. For example, you can use Ruby on Rails for one microservice and Django for another. 
+
+There are several free resources to learn and implement microservices. Here are some of them:
+
+1. [Master Spring Microservices with Spring Boot & Spring Cloud](https://www.udemy.com/course/microservices-with-spring-boot-and-spring-cloud/): This is a free course on Udemy that teaches the basics of Microservice architecture using Spring Boot and Spring Cloud. It provides hands-on experience in developing microservices [Source 1](https://medium.com/javarevisited/7-free-microservices-courses-for-java-programmers-c9b2f3a2ea7d).
+
+2. [Basics of Microservices](https://www.udemy.com/course/basics-of-microservices/): This is another free course on Udemy that provides a fundamental understanding of Microservices, its benefits, and how to design a highly available, fault-tolerant, secure, and scalable distributed system [Source 1](https://medium.com/javarevisited/7-free-microservices-courses-for-java-programmers-c9b2f3a2ea7d).
+
+3. [Microservices Full Course - Learn Microservices in 4 Hours](https://www.youtube.com/watch?v=V9vT2ZG0Y64): This is a free 4-hour long course on YouTube by Edureka that covers the basics of Microservice Architecture, its features, benefits, challenges, tools, etc [Source 1](https://medium.com/javarevisited/7-free-microservices-courses-for-java-programmers-c9b2f3a2ea7d).
+
+4. [Build Java Microservices with Spring Boot and Spring Cloud](https://www.udemy.com/course/build-java-microservices-with-spring-boot-and-spring-cloud/): This free course on Udemy teaches how to build simple Microservices using Spring Boot and Spring Cloud [Source 1](https://medium.com/javarevisited/7-free-microservices-courses-for-java-programmers-c9b2f3a2ea7d).
+
+To implement microservices in Django and Rails, you need to separate each functionality of your application into different services. Each service should be independent and should communicate with others through APIs. For Django, you can break down your monolithic application into microservices by listing down all the possible modules in your site or app and then decompose them by business capability [Source 14](https://stackoverflow.com/questions/56585409/how-to-break-a-django-website-into-microservices). For Rails, you can add REST services that query the database and return data [Source 15](https://www.codementor.io/@devgraph/the-ruby-unbundled-series-add-microservices-to-your-rails-app-in-minutes-1iq74e1t9p).
+
+You can also learn more about microservices from various online communities and forums such as StackOverflow, GitHub, and others. These platforms often have detailed discussions and examples of real-world microservices implementations [Source 24](https://stackoverflow.com/questions/40467382/how-to-use-role-authorization-for-micro-services-architecture), [Source 26](https://github.com/diyframework/django-microservice).
+
+
+
