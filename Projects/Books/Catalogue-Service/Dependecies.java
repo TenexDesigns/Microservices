8c396ae1a4318245c8e24644bef903b3b7d82519
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-28 22:23:00 +0300

    Create Dependecies.java

---
+++ Projects/Books/Catalogue-Service/Dependecies.java
@@ -0,0 +1,46 @@
+
+DependenciesADD ...
+
+Spring Web WEB
+Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.
+
+
+
+PostgreSQL Driver SQL
+A JDBC and R2DBC driver that allows Java programs to connect to a PostgreSQL database using standard, database independent Java code.
+
+
+
+Lombok DEVELOPER TOOLS
+Java annotation library which helps to reduce boilerplate code.
+
+
+Spring Data JPA SQL
+Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.
+
+
+Spring Boot DevTools DEVELOPER TOOLS
+Provides fast application restarts, LiveReload, and configurations for enhanced development experience.
+
+Spring Data JDBC SQL
+Persist data in SQL stores with plain JDBC using Spring Data.
+
+
+Flyway Migration SQL
+Version control for your database so you can migrate from any version (incl. an empty database) to the latest version of the schema.
+
+
+Validation I/O
+Bean Validation with Hibernate validator.
+
+
+OpenFeign SPRING CLOUD ROUTING
+Declarative REST Client. OpenFeign creates a dynamic implementation of an interface decorated with JAX-RS or Spring MVC annotations.
+
+
+Eureka Discovery Client SPRING CLOUD DISCOVERY
+A REST based service for locating services for the purpose of load balancing and failover of middle-tier servers.
+
+
+
+GENERATEEXPLORESHARE...
