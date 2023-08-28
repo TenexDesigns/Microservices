abfc0bc264fad28d4c7815c04625dd8f779dde0e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-28 22:31:49 +0300

    Create ReviewServiceApplication.java

---
+++ Projects/Books/Review-Microservice/ReviewServiceApplication.java
@@ -0,0 +1,9 @@
+@SpringBootApplication
+@EnableFeignClients
+public class ReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServiceApplication.class, args);
+	}
+
+}
