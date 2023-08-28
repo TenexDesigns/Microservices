33440553cd3a3c1bc3593c77f7e0ae4ac15e9652
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-28 22:42:42 +0300

    Create Dependecies.java

---
+++ Projects/Books/Eureka_Server/Dependecies.java
@@ -0,0 +1,10 @@
+Only Two depencies are needed 
+
+
+Spring Web WEB
+Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.
+
+
+  
+Eureka Server SPRING CLOUD DISCOVERY
+spring-cloud-netflix Eureka Server.
