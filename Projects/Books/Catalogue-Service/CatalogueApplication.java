5d74bdd2048fa8a39b9f52ebb66f3a8cc3a3ff19
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-28 22:17:24 +0300

    Create CatalogueApplication.java

---
+++ Projects/Books/Catalogue-Service/CatalogueApplication.java
@@ -0,0 +1,9 @@
+@SpringBootApplication
+@EnableFeignClients
+public class CatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogueApplication.class, args);
+	}
+
+}
