21f438ae41e979576e1c19069af61aac8bee8f58
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-29 12:12:19 +0300

    Create Dependecies_for_Api_Gateway.java

---
+++ Projects/Books/z_API-Gateway/Dependecies_for_Api_Gateway.java
@@ -0,0 +1,11 @@
+You only need two depencies
+
+
+Gateway SPRING CLOUD ROUTING
+Provides a simple, yet effective way to route to APIs and provide cross cutting concerns to them such as security, monitoring/metrics, and resiliency.
+
+
+
+  
+Eureka Discovery Client SPRING CLOUD DISCOVERY
+A REST based service for locating services for the purpose of load balancing and failover of middle-tier servers.
